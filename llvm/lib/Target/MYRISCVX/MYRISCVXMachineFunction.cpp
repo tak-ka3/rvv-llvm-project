e703d279b8edceb951a3c50c4ac28794c5f37efe
msyksphinz <msyksphinz.dev@gmail.com>
2020-06-20 12:29:35 +0900

    Add MYRISCVXMachineFunction

---
+++ llvm/lib/Target/MYRISCVX/MYRISCVXMachineFunction.cpp
@@ -0,0 +1,24 @@
+//===-- MYRISCVXMachineFunctionInfo.cpp - Private data used for MYRISCVX --===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "MYRISCVXMachineFunction.h"
+
+#include "MYRISCVXInstrInfo.h"
+#include "MYRISCVXSubtarget.h"
+#include "llvm/IR/Function.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
+
+using namespace llvm;
+
+bool FixGlobalBaseReg;
+
+MYRISCVXFunctionInfo::~MYRISCVXFunctionInfo() {}
+
+void MYRISCVXFunctionInfo::anchor() { }
---
+++ llvm/lib/Target/MYRISCVX/MYRISCVXMachineFunction.h
@@ -0,0 +1,52 @@
+//===-- MYRISCVXMachineFunctionInfo.h - Private data used for MYRISCVX ----*- C++ -*-=//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===------------------------------------------------------------------------------===//
+//
+// This file declares the MYRISCVX specific subclass of MachineFunctionInfo.
+//
+//===------------------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_MYRISCVX_MYRISCVXMACHINEFUNCTION_H
+#define LLVM_LIB_TARGET_MYRISCVX_MYRISCVXMACHINEFUNCTION_H
+
+#include "llvm/CodeGen/MachineFrameInfo.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/MachineMemOperand.h"
+#include "llvm/CodeGen/PseudoSourceValue.h"
+#include "llvm/CodeGen/TargetFrameLowering.h"
+#include "llvm/Target/TargetMachine.h"
+#include <map>
+
+namespace llvm {
+
+// @{ MYRISCVXMachineFunction_h_MYRISCVXFunctionInfo
+class MYRISCVXFunctionInfo : public MachineFunctionInfo {
+  // @} MYRISCVXMachineFunction_h_MYRISCVXFunctionInfo
+ public:
+  MYRISCVXFunctionInfo(MachineFunction& MF)
+      : MF(MF),
+        VarArgsFrameIndex(0)
+  {}
+
+  ~MYRISCVXFunctionInfo();
+
+  int getVarArgsFrameIndex() const { return VarArgsFrameIndex; }
+  void setVarArgsFrameIndex(int Index) { VarArgsFrameIndex = Index; }
+
+ private:
+  virtual void anchor();
+
+  MachineFunction& MF;
+
+  /// VarArgsFrameIndex - FrameIndex for start of varargs area.
+  int VarArgsFrameIndex;
+};
+
+} // end of namespace llvm
+
+#endif // MYRISCVX_MACHINE_FUNCTION_INFO_H
