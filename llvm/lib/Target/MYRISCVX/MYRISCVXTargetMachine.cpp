ee3958adb0f156d9a74a38e6a3911c6220a1e36b
msyksphinz <msyksphinz.dev@gmail.com>
2021-04-17 13:54:34 +0900

    Add MYRISCVXTargetMachine

--- llvm/lib/Target/MYRISCVX/MYRISCVXTargetMachine.cpp
+++ llvm/lib/Target/MYRISCVX/MYRISCVXTargetMachine.cpp
@@ -1,4 +1,4 @@
-//===-- MYRISCVXTargetMachine.cpp - Define TargetMachine for MYRISCVX -------------===//
+//===-- MYRISCVXTargetMachine.cpp - Define TargetMachine for MYRISCVX ------===//
 //
 //                     The LLVM Compiler Infrastructure
 //
@@ -11,7 +11,9 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "MYRISCVX.h"
 #include "MYRISCVXTargetMachine.h"
+#include "MYRISCVXTargetObjectFile.h"
 
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/CodeGen/Passes.h"
@@ -20,7 +22,120 @@
 
 using namespace llvm;
 
-#define DEBUG_TYPE "MYRISCVX"
-
+// @{MYRISCVXTargetMachine_cpp_LLVMInitializeMYRISCVXTarget
 extern "C" void LLVMInitializeMYRISCVXTarget() {
+  // MYRISCVXターゲットの登録
+  RegisterTargetMachine<MYRISCVX32TargetMachine> X(getTheMYRISCVX32Target());
+  RegisterTargetMachine<MYRISCVX64TargetMachine> Y(getTheMYRISCVX64Target());
+}
+// @}MYRISCVXTargetMachine_cpp_LLVMInitializeMYRISCVXTarget
+
+
+// @{MYRISCVXTargetMachine_cpp_computeDataLayout
+// MYRISCVXターゲットマシンのデータレイアウトを決める
+static std::string computeDataLayout(const Triple &TT, StringRef CPU,
+                                     const TargetOptions &Options) {
+  std::string Ret = "";
+  Ret += "e";
+  Ret += "-m:m";
+
+  // ポインタのサイズを指定する. RV32の場合は32ビット, RV64の場合は64ビット
+  if (TT.isArch64Bit()) {
+    Ret += "-p:64:64";
+  } else {
+    Ret += "-p:32:32";
+  }
+
+  // 8ビットと16ビット変数は32ビットにアラインする
+  // 64ビット変数は64ビットにアラインする
+  Ret += "-i8:8:32-i16:16:32-i64:64";
+  // 整数レジスタ・スタックのアライメント
+  if (TT.isArch64Bit()) {
+    Ret += "-n64-S128";
+  } else {
+    Ret += "-n32-S64";
+  }
+
+  return Ret;
+}
+// @}MYRISCVXTargetMachine_cpp_computeDataLayout
+
+
+// @{ MYRISCVXTargetMachine_cpp_getEffectiveRelocModel
+// MYRISCVXの有効なリロケーションモデルを返す
+// *RMを返すので、殆どの場合にはオプションで指定したモデルがそのまま使用される
+static Reloc::Model getEffectiveRelocModel(bool JIT,
+                                           Optional<Reloc::Model> RM) {
+  if (!RM.hasValue() || JIT)
+    return Reloc::Static;
+  return *RM;
+}
+// @} MYRISCVXTargetMachine_cpp_getEffectiveRelocModel
+
+
+// @{ MYRISCVXTargetMachine_cpp_MYRISCVXTargetMachine
+// @{ MYRISCVXTargetMachine_cpp_getEffectiveCodeModel
+// MYRISCVXTargetMachineのコンストラクタ：
+//   MYRISCVX32TargetMachine()とMYRISCVX64TargetMachine()から呼び出される
+MYRISCVXTargetMachine::MYRISCVXTargetMachine(const Target &T, const Triple &TT,
+                                             StringRef CPU, StringRef FS,
+                                             const TargetOptions &Options,
+                                             Optional<Reloc::Model> RM,
+                                             Optional<CodeModel::Model> CM,
+                                             CodeGenOpt::Level OL, bool JIT)
+    : LLVMTargetMachine(T, computeDataLayout(TT, CPU, Options), TT,
+                        CPU, FS, Options, getEffectiveRelocModel(JIT, RM),
+                        /* コードモデルの設定. デフォルトではSmall(=static), Medium(=PIC)も指定できる */
+                        getEffectiveCodeModel(CM, CodeModel::Small), OL),
+      // @} MYRISCVXTargetMachine_cpp_getEffectiveCodeModel
+      TLOF(std::make_unique<MYRISCVXTargetObjectFile>()),
+      ABI(MYRISCVXABIInfo::computeTargetABI(Options.MCOptions.getABIName())),
+      DefaultSubtarget(TT, CPU, /* TuneCPU */CPU, FS, *this) {
+  initAsmInfo();
+}
+// @}MYRISCVXTargetMachine_cpp_MYRISCVXTargetMachine
+
+MYRISCVXTargetMachine::~MYRISCVXTargetMachine() {}
+
+void MYRISCVX32TargetMachine::anchor() { }
+
+// @{MYRISCVXTargetMachine_cpp_MYRISCVX32_64TargetMachine
+// MYRISCVX32ターゲットのコンストラクタ. MYRISCVXTargetMachineインスタンスを生成する
+MYRISCVX32TargetMachine::MYRISCVX32TargetMachine(const Target &T, const Triple &TT,
+                                                 StringRef CPU, StringRef FS,
+                                                 const TargetOptions &Options,
+                                                 Optional<Reloc::Model> RM,
+                                                 Optional<CodeModel::Model> CM,
+                                                 CodeGenOpt::Level OL, bool JIT)
+    : MYRISCVXTargetMachine(T, TT, CPU, FS, Options, RM, CM, OL, JIT) {}
+
+void MYRISCVX64TargetMachine::anchor() { }
+
+// MYRISCVX64ターゲットのコンストラクタ. MYRISCVXTargetMachineインスタンスを生成する
+MYRISCVX64TargetMachine::MYRISCVX64TargetMachine(const Target &T, const Triple &TT,
+                                                 StringRef CPU, StringRef FS,
+                                                 const TargetOptions &Options,
+                                                 Optional<Reloc::Model> RM,
+                                                 Optional<CodeModel::Model> CM,
+                                                 CodeGenOpt::Level OL, bool JIT)
+    : MYRISCVXTargetMachine(T, TT, CPU, FS, Options, RM, CM, OL, JIT) {}
+// @}MYRISCVXTargetMachine_cpp_MYRISCVX32_64TargetMachine
+
+namespace {
+//@MYRISCVXPassConfig {
+/// MYRISCVX Code Generator Pass コンフィグレーションオプション.
+class MYRISCVXPassConfig : public TargetPassConfig {
+ public:
+  MYRISCVXPassConfig(MYRISCVXTargetMachine &TM, PassManagerBase &PM)
+      : TargetPassConfig(TM, PM) {}
+
+  MYRISCVXTargetMachine &getMYRISCVXTargetMachine() const {
+    return getTM<MYRISCVXTargetMachine>();
+  }
+
+};
+} // namespace
+
+TargetPassConfig *MYRISCVXTargetMachine::createPassConfig(PassManagerBase &PM) {
+  return new MYRISCVXPassConfig(*this, PM);
 }
--- llvm/lib/Target/MYRISCVX/MYRISCVXTargetMachine.h
+++ llvm/lib/Target/MYRISCVX/MYRISCVXTargetMachine.h
@@ -7,11 +7,79 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This file declares the Cpu0 specific subclass of TargetMachine.
+// This file declares the MYRISCVX specific subclass of TargetMachine.
 //
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIB_TARGET_MYRISCVX_MYRISCVXTARGETMACHINE_H
 #define LLVM_LIB_TARGET_MYRISCVX_MYRISCVXTARGETMACHINE_H
+
+#include "MCTargetDesc/MYRISCVXABIInfo.h"
+#include "MYRISCVXSubtarget.h"
+#include "llvm/CodeGen/Passes.h"
+#include "llvm/CodeGen/SelectionDAGISel.h"
+#include "llvm/CodeGen/TargetFrameLowering.h"
+#include "llvm/Target/TargetMachine.h"
+
+// @{MYRISCVXTargetMachine_h_MYRISCVXTargetMachine
+namespace llvm {
+class formatted_raw_ostream;
+class MYRISCVXRegisterInfo;
+
+// MYRISCVXTargetMachineはLLVMTargetMachineを継承して定義される
+class MYRISCVXTargetMachine : public LLVMTargetMachine {
+  std::unique_ptr<TargetLoweringObjectFile> TLOF;
+  MYRISCVXABIInfo ABI;
+  MYRISCVXSubtarget DefaultSubtarget;
+// @}MYRISCVXTargetMachine_h_MYRISCVXTargetMachine
+
+  mutable StringMap<std::unique_ptr<MYRISCVXSubtarget>> SubtargetMap;
+ public:
+  MYRISCVXTargetMachine(const Target &T, const Triple &TT, StringRef CPU,
+                        StringRef FS, const TargetOptions &Options,
+                        Optional<Reloc::Model> RM,
+                        Optional<CodeModel::Model> CM,
+                        CodeGenOpt::Level OL, bool JIT);
+  ~MYRISCVXTargetMachine() override;
+
+  const MYRISCVXSubtarget *getSubtargetImpl(const Function &F) const override {
+    return &DefaultSubtarget;
+  }
+
+  // Pass Pipeline Configuration
+  TargetPassConfig *createPassConfig(PassManagerBase &PM) override;
+
+  TargetLoweringObjectFile *getObjFileLowering() const override {
+    return TLOF.get();
+  }
+  const MYRISCVXABIInfo &getABI() const { return ABI; }
+};
+
+// @{MYRISCVXTargetMachine_h_MYRISCVXTargetMachine_32_64
+/// MYRISCVX32TargetMachine - MYRISCVX32 ターゲットマシン.
+///
+class MYRISCVX32TargetMachine : public MYRISCVXTargetMachine {
+  virtual void anchor();
+ public:
+  MYRISCVX32TargetMachine(const Target &T, const Triple &TT, StringRef CPU,
+                          StringRef FS, const TargetOptions &Options,
+                          Optional<Reloc::Model> RM,
+                          Optional<CodeModel::Model> CM,
+                          CodeGenOpt::Level OL, bool JIT);
+};
+
+/// MYRISCVX64TargetMachine - MYRISCVX64 ターゲットマシン.
+///
+class MYRISCVX64TargetMachine : public MYRISCVXTargetMachine {
+  virtual void anchor();
+ public:
+  MYRISCVX64TargetMachine(const Target &T, const Triple &TT, StringRef CPU,
+                          StringRef FS, const TargetOptions &Options,
+                          Optional<Reloc::Model> RM,
+                          Optional<CodeModel::Model> CM,
+                          CodeGenOpt::Level OL, bool JIT);
+};
+// @}MYRISCVXTargetMachine_h_MYRISCVXTargetMachine_32_64
+} // End llvm namespace
 
 #endif // LLVM_LIB_TARGET_MYRISCVX_MYRISCVXTARGETMACHINE_H
